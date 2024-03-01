40eaf91f8e35de788c873b248a1e04d4d8173154
Anwarul07 <mdanwarul064@gmail.com>
2024-03-01 23:19:17 +0530

    Update calculator.js

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let string = '';
+let input = document.querySelector("input");
+let buttons = document.querySelectorAll(".btn");
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+            console.log(string);
+            input.value=string;
+        } else {
+            string = string + e.target.innerHTML;
+            input.value = string;
+            console.log(e.target)
+        }
+        if(e.target.innerHTML==="C"){
+            input.value='';
+            string=''
+        }
+    })
+});
